266950866691e18ad6981614cc4f1baab35c69a5
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 18:28:03 -0500

    success request file

--- src/helpers/common/requestHelper.js
+++ src/helpers/common/requestHelper.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
 
-export const requestHelper = (formData) => axios[formData.get('method')
-  .toLowerCase()](formData.get('url'), formData.get('data'));
+export const requestHelper = (formData) => {
+  const method = formData.get('method');
+  const requestData = formData.get('data') || {};
+  const url = formData.get('url');
+  let request = null;
+  switch (method) {
+    case 'get':
+      request = axios[method.toLowerCase()](url);
+      break;
+    case 'post':
+      request = axios[method.toLowerCase()](url, requestData);
+      break;
+    default:
+      break;
+  }
+  return request;
+};
 
 export default {};
