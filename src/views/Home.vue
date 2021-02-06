4b0bb4a7530f55c22f119ac1951ef8a96c8d43b9
BuildTools <unconfigured@null.spigotmc.org>
2021-02-06 15:56:27 -0500

    Che eliminado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
-    <a href="#" @click="che">CLICK</a>
+    <a href="#" @click="getCandidates">CLICK</a>
     <VotingCard/>
   </div>
 </template>
 
 <script>
 import VotingCard from '@/components/common/VotingCard.vue';
-import { homeTypes } from '@/store/modules/home/homeTypes';
 import { mapActions } from 'vuex';
+import { homeTypes } from '@/store/modules/home/homeTypes';
 
 export default {
   name: 'Home',
@@ -19,9 +19,6 @@ export default {
     ...mapActions(homeTypes.PATH, {
       getCandidates: homeTypes.actions.GET_CANDIDATES,
     }),
-    che() {
-      this.getCandidates();
-    },
   },
   mounted() {
   },
